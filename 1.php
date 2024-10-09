9736915f6eb2d10276475ab996401053d0061074
Nicole Ronquillo <nicolethereseronquillo@gmail.com>
2024-10-09 22:36:26 +0800

    Number 1

--- 1.php
+++ 1.php
@@ -1,8 +1,15 @@
 <?php
+$i=1;
+while ($i <=10){
+    echo $i." ";
+    $i++;
+}
+
+echo "\n";
 
 $i = 1;
 while($i <= 20){
-    if($i % 2 ==0){
+    if($i % 2==0){
         echo $i." ";
     }
     $i++;
