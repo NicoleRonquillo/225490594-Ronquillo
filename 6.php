5f2370a03419f902c26f3bd02a145bc53e1aa1bd
Nicole Ronquillo <nicolethereseronquillo@gmail.com>
2024-10-09 22:58:56 +0800

    Number 2

---
+++ 6.php
@@ -0,0 +1,13 @@
+<?php
+$favoriteMovies = array(
+    "1. The Shawshank Redemption",
+    "2. Inception",
+    "3. The Dark Knight",
+    "4. Interstellar",
+    "5. Parasite"
+);
+
+foreach ($favoriteMovies as $movie) {
+    echo $movie . "\n";
+}
+?>
